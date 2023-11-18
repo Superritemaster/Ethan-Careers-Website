4b5becfeebb7a4ec722d806513f47057e38f55f9
Superritemaster <118483531+Superritemaster@users.noreply.github.com>
2023-11-18 11:08:44 +0000

    used advanced html forms and saved applications to DB

--- database.py
+++ database.py
@@ -34,7 +34,22 @@ def load_job_from_db(id):
         return None
       else:
         return row
-    
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query =   query = text(f"INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES ({job_id}, '{data['full_name']}', '{data['email']}', '{data['linkedin_url']}', '{data['education']}', '{data['work_experience']}', '{data['resume_url']}')")
+            
+          
+    conn.execute(query)
+
+
+
+
+
+
+
+
+ 
 
       
     
